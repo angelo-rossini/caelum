05fcfe2858efa79a4df43391c22e8cf05ed2aeff
angelo rossini <angelorossini96@gmail.com>
2022-05-10 16:54:45 -0300

    Add files via upload

---
+++ desafio1.java
@@ -0,0 +1,16 @@
+import java.util.TreeSet;
+
+public class desafio1 {
+	
+	public static void main(String[] args) {
+		TreeSet<Integer> conjunto = new TreeSet<>();
+		for (int i = 1; i <= 1000; i++) {
+			conjunto.add(i);
+		}
+
+		for (Integer i : conjunto.descendingSet()) {
+			System.out.print(i + " ");
+		}
+	}
+
+}
---
+++ ex1_1.java
@@ -0,0 +1,26 @@
+import java.util.HashSet;
+import java.util.Collection;
+
+public class ex1_1 {
+	
+	public static void main(String[] args) {
+		System.out.println("Iniciando...");
+		Collection<Integer> teste = new HashSet<>();
+		long inicio = System.currentTimeMillis();
+
+		int total = 30000;
+
+		for (int i = 0; i < total; i++) {
+			teste.add(i);
+		}
+
+		for (int i = 0; i < total; i++) {
+			teste.contains(i);
+		}
+
+		long fim = System.currentTimeMillis();
+		long tempo = fim - inicio;
+		System.out.println("Tempo gasto: " + tempo);
+	}
+
+}
